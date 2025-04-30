abedef7d6bb87ec7c17603dbe02d688a741c4fcc
ashwintenneti <159220821+ashwintenneti@users.noreply.github.com>
2025-04-30 13:32:51 +1000

    Update service-worker.js

--- simple-pwa/service-worker.js
+++ simple-pwa/service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'simple-pwa-cache-v1';
 const urlsToCache = [
-  '/Programming-for-the-web/simple-pwa/index.html',
-  '/Programming-for-the-web/simple-pwa/manifest.json',
-  '/Programming-for-the-web/simple-pwa/app.js',
-  '/Programming-for-the-web/simple-pwa/service-worker.js',
-  '/Programming-for-the-web/simple-pwa/icon.png'
+  '/Web-Programming/simple-pwa/index.html',
+  '/Web-Programming/simple-pwa/manifest.json',
+  '/Web-Programming/simple-pwa/app.js',
+  '/Web-Programming/simple-pwa/service-worker.js',
+  '/Web-Programming/simple-pwa/icon.png'
 ];
 
 // Install the service worker
